e5d2e2dd72c17106b0334f0ac33ef7594bfe2fc3
Graham Daws <graham.daws@gmail.com>
2013-06-13 00:38:30 +0100

    Export FrameInputStream, FrameOutputStream and util

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
 module.exports = {
-    Client: require("./lib/client")
+    
+    Client: require("./lib/client"),
+    
+    FrameInputStream: require("./lib/frame_input_stream"),
+    FrameOutputStream: require("./lib/frame_output_stream"),
+    
+    util: require("./lib/util")
 };
