b267db3d5800ff3211866c2f63822af10c640176
Graham Daws <graham.daws@gmail.com>
2013-06-26 00:52:25 +0100

    Add stompit.connect function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
+var helper = require("./lib/helper");
+
 module.exports = {
     
     Client: require("./lib/client"),
     
     FrameInputStream: require("./lib/frame_input_stream"),
     FrameOutputStream: require("./lib/frame_output_stream"),
+    
+    connect: helper.connect
 };
