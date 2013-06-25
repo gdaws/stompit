1e102fcd1e67cea19ff35061268951dcc257f624
Graham Daws <graham.daws@gmail.com>
2013-06-25 23:35:26 +0100

    Remove util export

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 module.exports = {
     
     Client: require("./lib/client"),
-        
+    
     FrameInputStream: require("./lib/frame_input_stream"),
     FrameOutputStream: require("./lib/frame_output_stream"),
-    
-    util: require("./lib/util")
 };
