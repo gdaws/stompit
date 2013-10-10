5ce06eec6473695bb56f072b1f21d63ddd101afa
Graham Daws <graham.daws@gmail.com>
2013-10-11 00:00:10 +0100

    Add code example of creating a transaction

---
+++ example/client/transaction.js
@@ -0,0 +1,32 @@
+var stompit = require('stompit');
+
+var connectParams = {
+    host: 'localhost',
+    port: 61613,
+    connectHeaders:{
+        host: 'mybroker'
+    }
+};
+
+stompit.connect(connectParams, function(error, client){
+    
+    if(error){
+        console.log('Unable to connect: ' + error.message);
+        return;
+    }
+    
+    var transaction = client.begin();
+    
+    transaction.send({'destination': '/queue/test'}).end('first');
+    transaction.send({'destination': '/queue/test'}).end('second');
+    
+    transaction.commit(); // or call transaction.abort()
+    
+    client.disconnect(function(error){
+        if(error){
+            console.log('Error while disconnecting: ' + error.message);
+            return;
+        }
+        console.log('Sent messages');
+    });
+});
