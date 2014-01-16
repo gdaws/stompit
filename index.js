78488042332f7e83637d083219236dc082ef496d
Graham Daws <graham.daws@gmail.com>
2014-01-16 23:17:05 +0000

    Run jshint in npm test command. Change code style.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
+/*jslint node: true, indent: 2, unused: true */
+
+'use strict';
 
 var Messaging = require("./lib/messaging");
 
 module.exports = {
-    
-    IncomingFrameStream: require("./lib/incoming_frame_stream"),
-    FrameOutputStream: require("./lib/frame_output_stream"),
-    
-    Client: require("./lib/client"),
-    
-    Failover: require("./lib/failover"),
-    Messaging: Messaging,
-    
-    connect: require("./lib/connect"),
-    
-    broker: function(){
-        var messaging = Object.create(Messaging.prototype);
-        Messaging.apply(messaging, arguments);
-        return messaging;
-    }
+
+  IncomingFrameStream: require("./lib/incoming_frame_stream"),
+  FrameOutputStream: require("./lib/frame_output_stream"),
+
+  Client: require("./lib/client"),
+
+  Failover: require("./lib/failover"),
+  Messaging: Messaging,
+
+  connect: require("./lib/connect"),
+
+  broker: function () {
+    var messaging = Object.create(Messaging.prototype);
+    Messaging.apply(messaging, arguments);
+    return messaging;
+  }
 };
